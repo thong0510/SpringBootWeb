f8becbe9ba6259461bff32d0ede480d2de76bdf5
thong0510 <thong0510@gmail.com>
2021-11-12 22:15:35 +0700

    create project

---
+++ src/main/java/com/fpt/salewebsite/SaleWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.salewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/salewebsite/SaleWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.salewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
